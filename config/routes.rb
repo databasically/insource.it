526298f97afce65399e064477dee7cadce000439
Willis F. Jackson III <raidercat@gmail.com>
2012-10-18 16:50:07 -0500

    routing and added js

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,11 @@ Insource::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static#home'
+  
+  match 'about' => 'static#about'
+  match 'contact' => 'static#contact'
+  match 'products' => 'static#products'
 
   # See how all your routes lay out with "rake routes"
 
