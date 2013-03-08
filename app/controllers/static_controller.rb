9a0617e1ddc3399700044bb2de7e409046a77aa1
Willis F. Jackson III <raidercat@gmail.com>
2013-03-08 16:25:18 -0600

    created dynamic title tags

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,15 +1,19 @@
 class StaticController < ApplicationController
 
   def home
+    @title = "Kansas City Small Business IT Support Services"
   end
   
   def contact
+    @title = "IT Experts that fix problems fast"
   end
   
   def products
+    @title = "Flexible business computer tech support solutions"
   end
   
   def about
+    @title = "Small Business IT Experts"
   end
   
 end
